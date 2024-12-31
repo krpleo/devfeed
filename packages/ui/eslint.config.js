1f97334395074fc2573a0ff308217382a14e724e
Kaiden Riley <kaiden.riley@pleo.io>
2024-12-31 19:18:18 +0100

    📦 add UI package

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,9 +1,11 @@
 import baseConfig from "@devfeed/eslint-config/base";
+import reactConfig from "@devfeed/eslint-config/react";
 
 /** @type {import('typescript-eslint').Config} */
 export default [
   {
-    ignores: [],
+    ignores: ["dist/**"],
   },
   ...baseConfig,
+  ...reactConfig,
 ];
