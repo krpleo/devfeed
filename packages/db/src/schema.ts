f0fa71ff5ba4d4070e772a7cff78972a5e3b74e6
Kaiden Riley <kaiden.riley@pleo.io>
2025-01-01 13:49:53 +0100

    🛰️ create db package

---
+++ packages/db/src/schema.ts
@@ -0,0 +1,9 @@
+import { pgTable } from "drizzle-orm/pg-core";
+
+export const User = pgTable("user", (t) => ({
+  id: t.uuid().notNull().primaryKey().defaultRandom(),
+  name: t.varchar({ length: 255 }),
+  email: t.varchar({ length: 255 }).notNull(),
+  emailVerified: t.timestamp({ mode: "date", withTimezone: true }),
+  image: t.varchar({ length: 255 }),
+}));
